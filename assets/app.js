c16b480527446198a6e40643c6c1bb6d29260a02
MisterPingouin <anthony.axeldisplay@gmail.com>
2024-01-20 05:28:42 +0100

    Optimisation Burger Menu

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './frontend/pages/Home';
+import Contact from './frontend/pages/Contact';
 import ErrorPage from './frontend/pages/ErrorPage';
 import './styles/app.scss';
 
@@ -9,6 +10,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
         'titlefont': ['PT Sans'],
         'subtitlefont': ['PT Mono'],
     },
-    extend: {},
+
+    maxWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+       },
+
+    extend: {
+        colors: {
+            coloryellow: '#fabd43',
+            colorbrown: '#',
+        },
+
+
+    },
   },
   plugins: [],
 }
