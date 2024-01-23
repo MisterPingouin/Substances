93cf5f0ae1f7aa0405e97bdcaa71ffc71a19d860
MisterPingouin <anthony.axeldisplay@gmail.com>
2024-01-23 16:27:32 +0100

    Few fixes front-end

--- assets/frontend/components/Conseils.js
+++ assets/frontend/components/Conseils.js
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from "react-router-dom"
-import {
-    Button,
-  } from "@material-tailwind/react";
-   
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Footer from "./Footer";
 
 const Conseils = () => {
   const [conseils, setConseils] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/conseils/list')
-      .then(response => {
+    axios
+      .get("/api/conseils/list")
+      .then((response) => {
         setConseils(response.data);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, []);
 
-    const renderWithLineBreaks = (text) => {
-    const lines = text.split('\n');
+  const renderWithLineBreaks = (text) => {
+    const lines = text.split("\n");
     return lines.map((line, index) => (
       <React.Fragment key={index}>
         {line}
@@ -30,33 +27,53 @@ const Conseils = () => {
 
   return (
     <>
-    <div className='text-3xl p-4 text-colorbrown mr-20 pr-14 space-y-2 mb-14 mt-4 font-bold w-2/3'>
-    {conseils.map((conseil, index) => (
+      <div className="text-3xl p-4 text-colorbrown mr-20 pr-14 space-y-2 mb-14 mt-4 font-bold w-2/3">
+        {conseils.map((conseil, index) => (
           <div key={index}>
-          <a href={`#conseil-${conseil.id}`}>{conseil.lien}</a>
-        </div>
-    ))}
-  </div>
-    <div>
-      {conseils.map((conseil, index) => (
-          <div key={index} id={`conseil-${conseil.id}`} className='flex flex-col items-center justify-center'>
-        {conseil.image && <img src={conseil.image} alt={conseil.titre} className='w-full max-w-4xl' />}
-        <div className='flex flex-col justify-center text-colorbrown items-center text-2xl font-subtitlefont w-2/3'>
-          <h1 className='text-6xl font-bold mt-20'>{conseil.titre}</h1>
-          <p className='mt-10 '>{conseil.sousDescription}</p>
-          <p className='mt-10 '>{renderWithLineBreaks(conseil.description)}</p>
-         {conseil.logoUrl && <img src={conseil.logoUrl} alt="Logo" className='w-full' />}
-          <p className=' text-sm '>{conseil.logoDescription}</p>
-          </div>  
-          <div className='flex flex-col space-y-4 mt-10 mb-10 text-colorbrown font-bold text-xl  w-2/3'>
-          <p className=''>{renderWithLineBreaks(conseil.description2)}</p>  
-          <Link to="/contact">
-          <Button className=" bg-coloryellow text-xl text-colorbrown">Me Contacter</Button>
-          </Link>
+            <a href={`#conseil-${conseil.id}`}>{conseil.lien}</a>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <div>
+        {conseils.map((conseil, index) => (
+          <div
+            key={index}
+            id={`conseil-${conseil.id}`}
+            className="flex flex-col items-center justify-center z-20 relative"
+          >
+            {conseil.image && (
+              <img
+                src={conseil.image}
+                alt={conseil.titre}
+                className="w-full max-w-4xl"
+              />
+            )}
+            <div className="flex flex-col justify-center text-colorbrown items-center text-3xl font-subtitlefont w-2/3">
+              <h1 className="text-6xl font-bold mt-20">{conseil.titre}</h1>
+              <p className="mt-10 ">{conseil.sousDescription}</p>
+              <p className="mt-10 ">
+                {renderWithLineBreaks(conseil.description)}
+              </p>
+              {conseil.logoUrl && (
+                <img src={conseil.logoUrl} alt="Logo" className="w-full" />
+              )}
+              <p className=" text-sm ">{conseil.logoDescription}</p>
+            </div>
+            <div className="flex flex-col space-y-4 mt-10 mb-10 text-colorbrown font-bold text-xl  w-2/3">
+              <p className="">{renderWithLineBreaks(conseil.description2)}</p>
+              <Link to="/contact">
+                <button
+                  type="button"
+                  class="inline-block rounded bg-coloryellow px-6 pb-2 pt-2.5  font-xl  leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca]"
+                >
+                  Me Contacter
+                </button>{" "}
+              </Link>
+            </div>
+          </div>
+        ))}
+        <Footer />
+      </div>
     </>
   );
 };
--- assets/frontend/components/Footer.js
+++ assets/frontend/components/Footer.js
@@ -5,7 +5,8 @@ import backgroundImageFooter from '../../images/FormeFooter.svg';
 const Footer = () => {
   return (
     <footer className="text-colorbrown  mx-auto relative">
-    <div className="text-colorbrown border-t border-black w-11/12 mx-auto relative z-10">
+      <div className='m-10'>
+    <div className="text-colorbrown border-t border-black w-[95%] mb-8 mx-auto relative z-10">
       </div>
       <div className='absolute flex justify-center items-center bottom-0 right-0 left-0'>
         <img src={backgroundImageFooter} alt="Background" className="w-full" />
@@ -14,21 +15,22 @@ const Footer = () => {
       <div className="py-6 relative"> 
         <div className="flex flex-col md:flex-row justify-between">
           <div>
-            <div className="flex flex-col ml-6 mb-4 text-2xl font-bold text-black">
+            <div className="flex flex-col ml-6 mb-4 text-3xl font-bold text-black space-y-1">
               <a href="/contact" className='hover:text-coloryellow'>Contact</a>
               <a href="/formation" className='hover:text-coloryellow' >Formation produit</a>
               <a href="/conseil"  className='hover:text-coloryellow'>Conseil & Coaching</a>
               <a href="/atelier"  className='hover:text-coloryellow'>Ateliers</a>
               <a href="#" className='hover:text-coloryellow'>A propos</a>
             </div>
-            <div className="ml-6 flex text-xl space-x-12 text-black ">
+            <div className="ml-6 flex text-xl space-x-12 text-black mb-36 pt-4">
               <a href="#" className='hover:text-gray-800'>Mentions légales</a>
               <a href="#" className='hover:text-gray-800'>Plan de site</a>
             </div>
           </div>
-          <div className="ml-auto self-end items-end mr-6">
+          <div className="ml-auto self-end items-end mr-6 mb-36">
             <img src={logo} alt="Logo" className="min-h-12" />
           </div>
+        </div>
         </div>
       </div>
     </footer>
