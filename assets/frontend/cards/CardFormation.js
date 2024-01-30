d4f67606251080aa94e8d1cece8d2e667f392785
MisterPingouin <anthony.axeldisplay@gmail.com>
2024-01-30 05:23:16 +0100

    test new carousel home

--- assets/frontend/cards/CardFormation.js
+++ assets/frontend/cards/CardFormation.js
@@ -12,18 +12,18 @@ import {
    
   export function CardFormation() {
     return (
-      <Card className="mt-6 w-96">
-        <CardHeader color="blue-gray" className="relative h-56">
+      <Card className="mt-6 h-full w-auto">
+        <CardHeader color="blue-gray">
           <img
             src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
             alt="card-image"
           />
         </CardHeader>
         <CardBody>
-          <Typography variant="h5" color="blue-gray" className="mb-2 text-2xl">
+          <Typography variant="h5" color="blue-gray" className="mb-2 text-5xl">
             Formation
           </Typography>
-          <Typography className="font-subtitlefont">
+          <Typography className="font-subtitlefont text-4xl">
           Morbi nec ligula ipsum.Maecenas non mollis quam, eget viverra nisi. Sed vitae magna iaculis, sollicitudin nunc sitamet, bibendum sem.
           </Typography>
         </CardBody>
