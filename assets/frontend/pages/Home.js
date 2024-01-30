ba6b091ec4800d54bb2c07adbfe8067bc48db909
MisterPingouin <anthony.axeldisplay@gmail.com>
2024-01-30 12:07:33 +0100

    footer desktop

--- assets/frontend/pages/Home.js
+++ assets/frontend/pages/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
       <Header />
       <main className="flex-grow justify-center items-center">
         <div className='flex flex-col justify-center items-center relative z-10'>
-        <h1 className="text-7xl p-4 text-colorbrown pt-20 mr-20 pr-14 font-bold w-2/3">Passion Substances Licites</h1>
-        <h2 className='text-4xl p-4 text-black mb-20 mr-20 pr-14 font-subtitlefont w-2/3'>Le monde des bières, des spiritueux et des sakés est un univers aux 1000 merveilles dans lequel on découvre toujours un nouveau savoir faire, une nouvelle personne, une nouvelle boisson qui nait de l'imagination et de l'expérience de personnes incroyables.</h2>
+        <h1 className="text-7xl p-4 text-colorbrown pt-20 mr-20 pr-14 font-bold w-2/3 lg:w-3/4">Passion Substances Licites</h1>
+        <h2 className='text-4xl p-4 text-black mb-20 mr-20 pr-14 font-subtitlefont w-2/3 lg:w-3/4'>Le monde des bières, des spiritueux et des sakés est un univers aux 1000 merveilles dans lequel on découvre toujours un nouveau savoir faire, une nouvelle personne, une nouvelle boisson qui nait de l'imagination et de l'expérience de personnes incroyables.</h2>
         </div>
         <div className='max-w-full flex flex-col h-[700px] bg-coloryellow justify-center items-center relative z-10'>
         <div className='flex flex-col justify-center items-center relative z-10'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,23 @@ module.exports = withMT({
             coloryellow: '#fabd43',
             colorbrown: '#342A2C',
         },
-
+        
+    screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+      
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+      
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+      
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+      
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+          }
 
     },
   },
