8d0318a6f9263972a7bd311474552e64b2132094
MisterPingouin <anthony.axeldisplay@gmail.com>
2024-03-07 15:44:20 +0100

    add credit photo on footer

--- assets/frontend/pages/Mention.js
+++ assets/frontend/pages/Mention.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Header from "../components/nav/Header";
+import Footer from "../components/Footer";
+
 
 const Mention = () => {
     return (
@@ -8,6 +11,7 @@ const Mention = () => {
             <h1 className="text-7xl p-4 text-colorbrown pt-20 mr-20 pr-14 font-bold w-2/3">Mention légale</h1>
             <h2 className='text-4xl p-4 text-black mb-20 mr-20 pr-14 font-subtitlefont w-2/3'>Blibloublou</h2>
           </main>
+          <Footer />
         </div>
       );
     };
